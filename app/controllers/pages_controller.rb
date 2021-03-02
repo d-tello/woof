0708ce654f7a8e00ad74043c9a4ae50ca55bf3ca
danburck <daniel.m.burckhardt@gmail.com>
2021-03-02 15:52:43 +0100

    add direct dogs to parks relation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @parks = current_user.dogs.first.parks
+    raise
   end
 end
