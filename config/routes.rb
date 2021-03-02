63a250a262c9b754bd8cc34bd870e4352bbaa397
Davide Tello <tello.davide@gmail.com>
2021-03-02 16:45:58 +0100

    Add possibility to edit the profile and upload a photo. Display the photo in the User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[ show ]
-=======
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: %i[show]
+
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
->>>>>>> master
 end
