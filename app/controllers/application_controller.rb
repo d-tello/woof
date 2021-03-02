aabf2f86738d4dc09dc0e88d605ca99947ff506e
Davide Tello <tello.davide@gmail.com>
2021-03-02 15:05:31 +0100

    Add parameter to permit to update username and picture too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username photo])
   end
 end
