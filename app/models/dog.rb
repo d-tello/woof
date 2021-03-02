ae673ed977c149864e87c9886636b61ce257d01f
Davide Tello <tello.davide@gmail.com>
2021-03-02 13:25:18 +0100

    Add the configuration

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,5 @@ class Dog < ApplicationRecord
   has_many :dogs_parks
   has_many :sniffs_as_sniffer, source: :sniffs, foreign_key: :sniffer_id
   has_many :sniffs_as_sniffed, source: :sniffs, foreign_key: :sniffed_id
+  has_many_attached :photos
 end
