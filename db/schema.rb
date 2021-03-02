16c3476abe32112a77bcef5cd91cd8a397de36c2
Davide Tello <tello.davide@gmail.com>
2021-03-02 15:30:50 +0100

    Add dogs info in the user profile page

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2021_03_02_111603) do
 
   # These are extensions that must be enabled in order to support this database
