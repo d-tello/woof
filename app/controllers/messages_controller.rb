3a93e74240c29549597a0643a364ccc6d5ae3187
Katja-Schroeder@web.de <Katja-Schroeder@web.de>
2021-03-02 15:20:07 +0100

    added a user_id to messages model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,23 @@
 class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.chatroom = @chatroom
+
+    if @message.save
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+
+      redirect_to chatroom_path(@chatroom)
+    else
+      render "chatrooms/show"
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
