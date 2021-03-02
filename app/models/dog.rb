da8ce05f1b53467646d62a7c08fcad3d875454ae
Katja-Schroeder@web.de <Katja-Schroeder@web.de>
2021-03-02 12:38:57 +0100

    validations on the dogmaodel

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,8 @@ class Dog < ApplicationRecord
   has_many :dogs_parks
   has_many :sniffs_as_sniffer, source: :sniffs, foreign_key: :sniffer_id
   has_many :sniffs_as_sniffed, source: :sniffs, foreign_key: :sniffed_id
+  validates :name, presence: true, length: { minimum: 2 };
+  validates :bredd, presence: true;
+  validates :age, presence: true, numericality: { only_integer: true };
+  validates :bio, presence: true, length: { maximum: 500 };
 end
