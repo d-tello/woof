9b456186669489cd44123da8e746cbbf30aa9eb9
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-03-02 12:00:18 +0100

    Update 20210302104202_create_chatrooms.rb

--- db/migrate/20210302104202_create_chatrooms.rb
+++ db/migrate/20210302104202_create_chatrooms.rb
@@ -1,7 +1,7 @@
 class CreateChatrooms < ActiveRecord::Migration[6.1]
   def change
     create_table :chatrooms do |t|
-      t.references :sniffs, null: false, foreign_key: true
+      t.references :sniff, null: false, foreign_key: true
 
       t.timestamps
     end
