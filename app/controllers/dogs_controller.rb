910d49366dbbb2df652f07ff52ff6550f8b0009b
danburck <daniel.m.burckhardt@gmail.com>
2021-03-03 16:33:25 +0100

    add dog#show route controller and basic view

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,5 @@
+class DogsController < ApplicationController
+  def show
+    @dog = Dog.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: %i[show]
+  resources :users, only: %i[show] do
+    resources :dogs, only: :show
+  end
 
   resources :chatrooms, only: :show do
     resources :messages, only: :create
