0bc16a3501e70ea7bb6eaf108fc470e9828800bb
Katja-Schroeder@web.de <Katja-Schroeder@web.de>
2021-03-03 12:15:30 +0100

    created a dogs controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_02_154056) do
 
   # These are extensions that must be enabled in order to support this database
@@ -45,10 +44,10 @@ ActiveRecord::Schema.define(version: 2021_03_02_154056) do
   end
 
   create_table "chatrooms", force: :cascade do |t|
-    t.bigint "sniff_id", null: false
+    t.bigint "sniffs_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["sniff_id"], name: "index_chatrooms_on_sniff_id"
+    t.index ["sniffs_id"], name: "index_chatrooms_on_sniffs_id"
   end
 
   create_table "dogs", force: :cascade do |t|
@@ -113,11 +112,9 @@ ActiveRecord::Schema.define(version: 2021_03_02_154056) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "chatrooms", "sniffs"
+  add_foreign_key "chatrooms", "sniffs", column: "sniffs_id"
   add_foreign_key "dogs", "users"
   add_foreign_key "dogs_parks", "dogs"
   add_foreign_key "dogs_parks", "parks"
