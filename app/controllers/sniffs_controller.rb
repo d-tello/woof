384e65f86a8f638bf802007434329c4dc4744f1e
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-03-04 16:15:10 +0100

    Update sniffs_controller.rb

--- app/controllers/sniffs_controller.rb
+++ app/controllers/sniffs_controller.rb
@@ -1,9 +1,4 @@
 class SniffsController < ApplicationController
-
-  # def new
-  #   @sniff = Sniff.new
-  # end
-
   def create
     @dog = Dog.find(params[:dog_id])
     @sniff = Sniff.new
@@ -14,9 +9,6 @@ class SniffsController < ApplicationController
       @chatroom = Chatroom.new(sniff: @sniff)
       @chatroom.save
            redirect_to dog_path(@dog)
-
-      # redirect_to chatroom_path(@chatroom)
-      # notice
     else
          render "dogs/show"
     end
