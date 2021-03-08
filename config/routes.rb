9c3527ee680315f48e0580d1e8f746b221d4a06d
danburck <daniel.m.burckhardt@gmail.com>
2021-03-08 17:27:17 +0100

    dogs show button with three states

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   end
 
   resources :dogs, only: :show do
-    resources :sniffs, only: :create
+    resources :sniffs, only: %i[create]
   end
 
   patch 'dogs/:id/toggle_ready_to_walk', to: 'dogs#toggle_ready_to_walk', as: 'dog_toggle_ready_to_walk'
