4437dd791466f944ab3ec59bf4b6393685b615c7
danburck <daniel.m.burckhardt@gmail.com>
2021-03-25 19:13:43 +0100

    add screenshot

--- test/system/trees_test.rb
+++ test/system/trees_test.rb
@@ -4,7 +4,7 @@ class TreesTest < ApplicationSystemTestCase
   test "visit home page and see dog" do
     login_as users(:dan)
     visit root_path
-    assert_selector "h1", text: "Dogs near you"
-    # assert_selector "p", text: "Grendel"
+    save_and_open_screenshot
+    assert_selector "p", text: "Grendel"
   end
 end
