0878122a172cfa1c835d63e48c0f2f214cafdbfb
danburck <daniel.m.burckhardt@gmail.com>
2021-03-26 09:42:42 +0100

    replace system test with simple visit login page

--- test/system/trees_test.rb
+++ test/system/trees_test.rb
@@ -1,14 +1,21 @@
 require "application_system_test_case"
 
 class TreesTest < ApplicationSystemTestCase
-  test "visit home page and see dog" do
-    login_as users(:dan)
+  # test "visit home page and see dog" do
+  #   login_as users(:dan)
+  #   visit root_path
+  #   puts "puts output"
+  #   puts html
+  #   Rails::logger.debug "loggerdebug output"
+  #   Rails::logger.debug html
+  #   assert_selector "p", text: "Grendel"
+  # end
+
+  test 'visit the login page' do
     visit root_path
-    save_and_open_screenshot
+    # save_and_open_screenshot
     puts "puts output"
     puts html
-    Rails::logger.debug "loggerdebug output"
-    Rails::logger.debug html
-    assert_selector "p", text: "Grendel"
+    assert_selector "h1", text: "Woof"
   end
 end
