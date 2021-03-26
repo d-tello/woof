33646005da6c3aebd3d3b047c3ceed266b277ed7
danburck <daniel.m.burckhardt@gmail.com>
2021-03-26 10:11:04 +0100

    add sniffs#index system test

--- test/system/trees_test.rb
+++ test/system/trees_test.rb
@@ -1,6 +1,13 @@
 require "application_system_test_case"
 
 class TreesTest < ApplicationSystemTestCase
+  test 'visit the login page' do
+    visit root_path
+    # puts "puts output"
+    # puts html
+    assert_selector "h1", text: "Woof"
+  end
+
   # test "visit home page and see dog" do
   #   login_as users(:dan)
   #   visit root_path
@@ -11,11 +18,10 @@ class TreesTest < ApplicationSystemTestCase
   #   assert_selector "p", text: "Grendel"
   # end
 
-  test 'visit the login page' do
-    visit root_path
+  test 'visit sniff#index page' do
+    login_as users(:dan)
+    visit sniffs_path
     # save_and_open_screenshot
-    puts "puts output"
-    puts html
-    assert_selector "h1", text: "Woof"
+    assert_selector "h1", text: "Look at who sniffed you"
   end
 end
